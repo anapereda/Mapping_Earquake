08eefab949599b1acf2daac939f0366739d4906f
anapereda <65132236+anapereda@users.noreply.github.com>
2020-08-05 13:10:59 -0700

    adding single markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,11 +2,14 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.24], 14);
 
-// We create the tile layer that will be the background of our map.
-//Add marker to Map for L.A, CA
-
+//  Add a marker to the map for Los Angeles, California.
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
