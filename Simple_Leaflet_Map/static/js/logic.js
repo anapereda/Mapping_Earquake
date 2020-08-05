7cf064d2a5f0e60b3be461e5789d1377b7440580
anapereda <65132236+anapereda@users.noreply.github.com>
2020-08-05 12:14:44 -0700

    simple leaflet map

---
+++ Simple_Leaflet_Map/static/js/logic.js
@@ -0,0 +1,19 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+//Add marker to Map for L.A, CA
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    //can be changed to a variety of ids; which will make the mal look different (different tile styles)
+	id: 'mapbox.streets',
+	accessToken: API_KEY
+});
+
+streets.addTo(map);
