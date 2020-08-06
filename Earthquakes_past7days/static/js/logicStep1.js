a6275fb7ea0ff0b3412e029f5052b2c4f11d19e9
anapereda <65132236+anapereda@users.noreply.github.com>
2020-08-06 16:06:51 -0700

    adding maps and marker

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,7 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-let map = L.map('mapid').setView([39.5, -98.5],3);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -23,15 +22,16 @@ let baseMaps = {
     "Satellite": satelliteStreets,
   };
 
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
 
-// Create the map object with a center and zoom level.
+L.control.layers(baseMaps).addTo(map)
 
-
-streets.addTo(map)
-//L.control.layers(baseMaps).addTo(map)
-
-//let url= 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-
-//d3.json(url).then(function(data){
-    //L.geoJson(data).addTo(map);
-//});
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
