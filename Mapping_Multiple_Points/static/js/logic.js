2ec35611b24e4a4a5f4f1dcd0fac46f16a171634
anapereda <65132236+anapereda@users.noreply.github.com>
2020-08-05 17:30:22 -0700

    Mapping Multiple Points

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,28 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+    L.circleMarket(city.location,{
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    //can be changed to a variety of ids; which will make the mal look different (different tile styles)
+	id: 'mapbox.streets',
+	accessToken: API_KEY
+});
+
+streets.addTo(map);
